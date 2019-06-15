cc24a908f49ffb6ca49f6cef281a346d238757d9
realVato <44781061+realVato@users.noreply.github.com>
2019-06-15 19:47:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
-/*
-    BMI Formula
-    mass / (height * height)
+/* CHALLENGE 1 
+
+Mark and John are trying to compare their BMI, which is calculated using the
+formula: BMI = mass / (height * height). (mass in kg and height in meter).
+
+1. Store Mark's and John's mass and height in variables.
+2. Calculate both their BMIs.
+3.Create a boolean variable containing information
+   about whether Mark has a higher BMI than John.
+4. Print a string to the console containing the variable from step 3. (Something
+   like "is Mark's BMI higher than John's? true").
 */
-var bmiMark, bmiJohn;
 
-// Stats
-var markHeight, markWeight;
+var johnHeight, johnMass, markHeight, markMass;
+johnHeight = 1.80;
+johnMass = 75;
+markHeight = 1.85;
+markMass = 60;
 
-markHeight = 1.80; // Meters
-markWeight = 72; // KG
+var johnBMI = johnMass / (johnHeight * johnHeight);
+var markBMI =  markMass / (markHeight * markHeight);
+console.log(johnBMI);
+console.log(markBMI);
 
-var johnHeight, johnWeight;
+var highestBMI = johnBMI >= markBMI;
 
-johnHeight = 1.82; // Meters
-johnWeight = 82; // KG
-
-
-// BMIs
-bmiMark = markWeight / (markHeight * markHeight);
-bmiJohn = johnWeight / (johnHeight * johnHeight);
-
-var BMI_WINNER = bmiMark > bmiJohn;
-
-console.log('Is Mark\'s BMI higher than John\'s?' + ' ' + BMI_WINNER);
-
+console.log('Is John\'s BMI higher than Mark\'s?' + ' ' + highestBMI);
